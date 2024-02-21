9f8f0c28c08de85b5d9c1cb50be393b345063f1b
wyatt-h <wyatt.hskr@gmail.com>
2024-02-21 12:47:37 -0600

    Twist function naming

--- internal/process/multicast.go
+++ internal/process/multicast.go
@@ -3,7 +3,7 @@ package process
 func (p *Process) multicast() {
 	for msg := range p.send {
 		for _, peer := range p.peers {
-			peer.Conn.Write([]byte(msg.ToString(p.self.Id)))
+			peer.Conn.Write([]byte(msg.ToNetworkMsg(p.self.Id)))
 		}
 	}
 }
